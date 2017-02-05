a1eb7788bd537da32b8e38ebe6d36a11e3709f0e
jackmtr <jackmtr@hotmail.com>
2017-02-05 09:47:27 -0800

    First commit

---
+++ src/com/contography/flashcards/Flashcard.java
@@ -0,0 +1,23 @@
+package com.contography.flashcards;
+/**
+ * Project : FlashCards 
+ * File : FlashCards.java
+ * Date : Feb 5, 2017
+ * Time : 9:36:06 AM
+ */
+
+/**
+ * @author Jackie
+ *
+ */
+public class Flashcard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
